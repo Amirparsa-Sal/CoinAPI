17d9ae997484dac35333bfcf785cd2b3e38a0bc0
Amirparsa-Sal <Amirparsa.s751@gmail.com>
2023-06-28 22:28:45 +0330

    make frontend pretty

--- frontend/script.js
+++ frontend/script.js
@@ -3,8 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const historyFormContainer = document.getElementById('historyFormContainer');
     const subscribeToggle = document.getElementById('subscribeToggle');
     const historyToggle = document.getElementById('historyToggle');
-    const subscriptionForm = document.getElementById('subscriptionForm');
-    const historyForm = document.getElementById('historyForm');
+    const subscriptionButton = document.getElementById('submitButton');
     const emailInput = document.getElementById('email');
     const coinNameInput = document.getElementById('coinName');
     const differencePercentInput = document.getElementById('differencePercent');
@@ -29,66 +28,68 @@ document.addEventListener('DOMContentLoaded', () => {
         historyToggle.classList.add('active');
       });
   
-    subscriptionForm.addEventListener('submit', (event) => {
-      event.preventDefault();
-      clearErrors();
-  
-      // Retrieve input values
-      const email = emailInput.value.trim();
-      const coinName = coinNameInput.value.trim();
-      const differencePercent = parseInt(differencePercentInput.value);
-  
-      // Validate input values
-      if (!isValidEmail(email)) {
-        displayError('Invalid email address');
-        return;
-      }
-  
-      if (coinName === '') {
-        displayError('Coin name is required');
-        return;
-      }
-  
-      if (isNaN(differencePercent) || differencePercent < 0 || differencePercent > 100) {
-        displayError('Difference percent must be a number between 0 and 100');
-        return;
-      }
-  
-      // Make API request to subscribe
-      // Replace `API_URL` with your actual API endpoint
-      const apiEndpoint = 'http://localhost:5001/api/subscribe';
-      const requestData = {
-        email: email,
-        coin_name: coinName,
-        difference_percent: differencePercent
-      };
-  
-      // Send the request using your preferred method (e.g., fetch, Axios, etc.)
-      // Example using fetch:
-      fetch(apiEndpoint, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(requestData)
-      })
-        .then(response => {
-          if (response.ok) {
-            // Subscription successful
-            // Handle success case (e.g., show success message, clear form, etc.)
-            subscriptionForm.reset();
-            alert('Subscription successful');
-          } else {
-            // Subscription failed
-            // Handle error case (e.g., display error message)
-            displayError('Subscription failed. Please try again.');
-          }
+      subscriptionButton.addEventListener('click', (event) => {
+        event.preventDefault();
+        clearErrors();
+    
+        // Retrieve input values
+        const email = emailInput.value.trim();
+        const coinName = coinNameInput.value.trim();
+        const differencePercent = parseInt(differencePercentInput.value);
+    
+        // Validate input values
+        if (!isValidEmail(email)) {
+            displayError('Invalid email address');
+            return;
+        }
+    
+        if (coinName === '') {
+            displayError('Coin name is required');
+            return;
+        }
+    
+        if (isNaN(differencePercent) || differencePercent < 0 || differencePercent > 100) {
+            displayError('Difference percent must be a number between 0 and 100');
+            return;
+        }
+    
+        // Make API request to subscribe
+        // Replace `API_URL` with your actual API endpoint
+        const apiEndpoint = 'http://localhost:5001/api/subscribe';
+        const requestData = {
+            email: email,
+            coin_name: coinName,
+            difference_percent: differencePercent
+        };
+    
+        // Send the request using your preferred method (e.g., fetch, Axios, etc.)
+        // Example using fetch:
+        fetch(apiEndpoint, {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(requestData)
         })
-        .catch(error => {
-          // Handle error case (e.g., display error message)
-          displayError('An error occurred. Please try again later.');
-          console.error(error);
-        });
+            .then(response => {
+            if (response.ok) {
+                // Subscription successful
+                // Handle success case (e.g., show success message, clear form, etc.)
+                emailInput.value = "";
+                coinNameInput.value = "";
+                differencePercentInput.value = "";
+                alert('Subscription successful');
+            } else {
+                // Subscription failed
+                // Handle error case (e.g., display error message)
+                displayError('Subscription failed. Please try again.');
+            }
+            })
+            .catch(error => {
+            // Handle error case (e.g., display error message)
+            displayError('An error occurred. Please try again later.');
+            console.error(error);
+            });
     });
   
     historyButton.addEventListener('click', () => {
@@ -150,6 +151,7 @@ document.addEventListener('DOMContentLoaded', () => {
       const errorElement = document.createElement('p');
       errorElement.classList.add('error');
       errorElement.textContent = message;
+      errorElement.style.color = "red";
       subscriptionFormContainer.appendChild(errorElement);
     }
   
